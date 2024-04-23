860502116abdd56ab828e257ef076b902c0d65a9
Eric Moore <EricGittaMoore@duck.com>
2024-04-23 21:01:16 +0800

    temp

--- projects/swimlane/ngx-charts/src/lib/nested-pie/nested-pie.component.ts
+++ projects/swimlane/ngx-charts/src/lib/nested-pie/nested-pie.component.ts
@@ -1,4 +1,3 @@
-import { BaseChartComponent } from '@swimlane/ngx-charts/common/base-chart.component';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -9,11 +8,53 @@ import {
   TemplateRef,
   ViewEncapsulation
 } from '@angular/core';
-import { LegendPosition } from '@swimlane/ngx-charts/common/types/legend.model';
+import { LegendOptions, LegendPosition } from '@swimlane/ngx-charts/common/types/legend.model';
+import { DataItem } from '@swimlane/ngx-charts/models/chart-data.model';
+import { ColorHelper } from '@swimlane/ngx-charts/common/color.helper';
+import { ViewDimensions } from '@swimlane/ngx-charts/common/types/view-dimension.interface';
+import { calculateViewDimensions } from '@swimlane/ngx-charts/common/view-dimensions.helper';
+import { ScaleType } from '@swimlane/ngx-charts/common/types/scale-type.enum';
+import { BaseChartComponent } from '@swimlane/ngx-charts/common/base-chart.component';
 
 @Component({
   selector: 'ngx-charts-nested-pie',
-  templateUrl: ``,
+  template: `
+    <ngx-charts-chart
+      [view]="[width, height]"
+      [showLegend]="legend"
+      [legendOptions]="legendOptions"
+      [activeEntries]="activeEntries"
+      [animations]="animations"
+      (legendLabelActivate)="onActivate($event, true)"
+      (legendLabelDeactivate)="onDeactivate($event, true)"
+      (legendLabelClick)="onClick($event)"
+    >
+      <svg:g [attr.transform]="translation" class="pie-chart chart">
+        <svg:g
+          ngx-charts-pie-series
+          [colors]="colors"
+          [series]="data"
+          [showLabels]="labels"
+          [labelFormatting]="labelFormatting"
+          [trimLabels]="trimLabels"
+          [maxLabelLength]="maxLabelLength"
+          [activeEntries]="activeEntries"
+          [innerRadius]="innerRadius"
+          [outerRadius]="outerRadius"
+          [explodeSlices]="explodeSlices"
+          [gradient]="gradient"
+          [animations]="animations"
+          [tooltipDisabled]="tooltipDisabled"
+          [tooltipTemplate]="tooltipTemplate"
+          [tooltipText]="tooltipText"
+          (dblclick)="dblclick.emit($event)"
+          (select)="onClick($event)"
+          (activate)="onActivate($event)"
+          (deactivate)="onDeactivate($event)"
+        />
+      </svg:g>
+    </ngx-charts-chart>
+  `,
   styleUrls: ['../common/base-chart.component.scss', './nested-pie.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -41,4 +82,124 @@ export class NestedPieComponent extends BaseChartComponent {
   @Output() deactivate = new EventEmitter();
 
   @ContentChild('tooltipTemplate') tooltipTemplate: TemplateRef<any>;
+
+  translation: string;
+  outerRadius: number;
+  innerRadius: number;
+  data: DataItem[];
+  colors: ColorHelper;
+  domain: string[];
+  dims: ViewDimensions;
+  legendOptions: LegendOptions;
+
+  update(): void {
+    super.update();
+
+    if (this.labels && this.hasNoOptionalMarginsSet()) {
+      this.margins = [30, 80, 30, 80];
+    } else if (!this.labels && this.hasNoOptionalMarginsSet()) {
+      // default value for margins
+      this.margins = [20, 20, 20, 20];
+    }
+
+    this.dims = calculateViewDimensions({
+      width: this.width,
+      height: this.height,
+      margins: this.margins,
+      showLegend: this.legend,
+      legendPosition: this.legendPosition
+    });
+
+    this.formatDates();
+
+    const xOffset = this.margins[3] + this.dims.width / 2;
+    const yOffset = this.margins[0] + this.dims.height / 2;
+    this.translation = `translate(${xOffset}, ${yOffset})`;
+    this.outerRadius = Math.min(this.dims.width, this.dims.height);
+    if (this.labels) {
+      // make room for labels
+      this.outerRadius /= 3;
+    } else {
+      this.outerRadius /= 2;
+    }
+    this.innerRadius = 0;
+    if (this.doughnut) {
+      this.innerRadius = this.outerRadius * (1 - this.arcWidth);
+    }
+
+    this.domain = this.getDomain();
+
+    // sort data according to domain
+    this.data = this.results.sort((a, b) => {
+      return this.domain.indexOf(a.name) - this.domain.indexOf(b.name);
+    });
+
+    this.setColors();
+    this.legendOptions = this.getLegendOptions();
+  }
+
+  getDomain(): string[] {
+    return this.results.map(d => d.label);
+  }
+
+  onClick(data: DataItem | string): void {
+    this.select.emit(data);
+  }
+
+  setColors(): void {
+    this.colors = new ColorHelper(this.scheme, ScaleType.Ordinal, this.domain, this.customColors);
+  }
+
+  getLegendOptions(): LegendOptions {
+    return {
+      scaleType: ScaleType.Ordinal,
+      domain: this.domain,
+      colors: this.colors,
+      title: this.legendTitle,
+      position: this.legendPosition
+    };
+  }
+
+  onActivate(item, fromLegend = false): void {
+    item = this.results.find(d => {
+      if (fromLegend) {
+        return d.label === item.name;
+      } else {
+        return d.name === item.name;
+      }
+    });
+
+    const idx = this.activeEntries.findIndex(d => {
+      return d.name === item.name && d.value === item.value && d.series === item.series;
+    });
+    if (idx > -1) {
+      return;
+    }
+
+    this.activeEntries = [item, ...this.activeEntries];
+    this.activate.emit({ value: item, entries: this.activeEntries });
+  }
+
+  onDeactivate(item, fromLegend = false): void {
+    item = this.results.find(d => {
+      if (fromLegend) {
+        return d.label === item.name;
+      } else {
+        return d.name === item.name;
+      }
+    });
+
+    const idx = this.activeEntries.findIndex(d => {
+      return d.name === item.name && d.value === item.value && d.series === item.series;
+    });
+
+    this.activeEntries.splice(idx, 1);
+    this.activeEntries = [...this.activeEntries];
+
+    this.deactivate.emit({ value: item, entries: this.activeEntries });
+  }
+
+  private hasNoOptionalMarginsSet(): boolean {
+    return !this.margins || this.margins.length <= 0;
+  }
 }
